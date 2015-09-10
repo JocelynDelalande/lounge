fe856268e9c53e42e1380a4add57bc4c1954c92e
Jocelyn Delande <jocelyn@crapouillou.net>
2015-09-10 03:55:47 +0200

    Implemented basic OTR support
    It uses https://github.com/arlolra/otr
    
    - pvkey is generated at each new otr conversation
    - lacks fingerprints viewing/validation
    - lacks persistent private key
    - lacks UI
    - lacks settings + UI
    - maybe needs some refacto (pluginification)

--- src/client.js
+++ src/client.js
@@ -12,6 +12,7 @@ var slate = require("slate-irc");
 var tls = require("tls");
 var EventEmitter = require('events').EventEmitter;
 var Helper = require("./helper");
+var OtrStore = require("./models/OtrStore");
 
 module.exports = Client;
 
@@ -33,7 +34,8 @@ var events = [
 	"quit",
 	"topic",
 	"welcome",
-	"whois"
+	"whois",
+	"otrmessage"
 ];
 var inputs = [
 	"action",
@@ -55,6 +57,7 @@ var inputs = [
 
 function Client(sockets, name, config) {
 	EventEmitter.call(this);
+	var client = this;
 	_.merge(this, {
 		activeChannel: -1,
 		config: config,
@@ -63,7 +66,7 @@ function Client(sockets, name, config) {
 		networks: [],
 		sockets: sockets
 	});
-	var client = this;
+	this.otrStore = new OtrStore(name, client);
 	crypto.randomBytes(48, function(err, buf) {
 		client.token = buf.toString("hex");
 	});
@@ -197,6 +200,10 @@ Client.prototype.connect = function(args) {
 			irc,
 			network
 		]);
+	});
+
+	this.on("join", function(args) {
+		this.otrStore.initChan(args);
 	});
 
 	irc.once("welcome", function() {
